d94e51b9bce29f992ca96c65f35767d42726de56
skate090961 <m4xwork@yandex.ru>
2023-06-14 12:47:04 +0400

    add html

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src="https://cdn6.aptoide.com/imgs/5/d/1/5d187923c2f94bf506bc9444ddf02516_icon.png" alt="logo"/>
+        </header>
+    );
+};
